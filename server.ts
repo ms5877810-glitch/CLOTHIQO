876e7ce63d37e6d3543ca7e19fe16b7f2fd3df50
ms5877810-glitch <ms5877810@gmail.com>
2026-09-09 09:22:56 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -98,7 +98,7 @@ function requireAdmin(req: Request, res: Response, next: NextFunction) {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;;
 
   app.use(express.json());
 
